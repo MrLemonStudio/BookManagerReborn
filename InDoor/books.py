0dacad3e39596f7ee7a8497f865a240f329f1714
PolarisCodingStudio <ujkl678@outlook.com>
2026-02-24 15:16:22 +0800

    Created function __new_book

--- InDoor/books.py
+++ InDoor/books.py
@@ -1,10 +1,11 @@
 from Tools.custom_errors import *
+from pathlib import Path
 from Tools.sqlite_new import SQLiteDatabaseManager
 import datetime
 import os
 
 class BooksManager:
-    def __init__(self,db_path:str,table_name:str,log_dir_path:str):
+    def __init__(self,db_path:str,table_name:str,log_dir_path:str|Path):
         run_time = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
         self.log_dir_path = log_dir_path
         if not os.path.exists(self.log_dir_path):
@@ -14,3 +15,7 @@ class BooksManager:
         self.table_name = table_name
         self.sqlite=SQLiteDatabaseManager(self.db_path)
         self.log_file=open(self.log_location,"w",encoding="utf-8",errors="ignore")
+
+    def __new_book(self,book_name:str,book_id:int|float|str,book_price:int|float|None=None,author:str|None=None,picture:str|Path|None=None)->bool:
+
+        return True
